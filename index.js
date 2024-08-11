45896d91ee5bfe47e3826f576676c2fb31c29000
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-11 18:59:33 +0530

    Spread & rest operator

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// Create a function that return a function
-
-var createHelloWorld = function() {
-    return function(...args) {
-        return "Hello World"    
+//pass the element using rest(...) method
+function add(...arr){
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+            sum = sum + arr[i];
     }
-};
+    return sum;
+}
 
-var a = createHelloWorld();
-console.log(a());
+let arr = [1,2,3,4,5,6,7,8,9,10];
+//copy the array 1 into array 2 using spread operator(...)
+let arr2 = [10,20,30 , ...arr];
+
+console.log("New array is " , arr2);
+
+
+console.log("Addition of array is ", add(1,2,3,4,5,6,7,8,9,10));
