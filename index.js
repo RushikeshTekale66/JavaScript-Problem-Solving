ba64dbbf54022f4151ebc8b9811df356469908e9
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-21 12:43:30 +0530

    Print array elements

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function handle(){
-    click = !click;
-    return click;
-}
 
-let click = true;
+const arr = [8, 7, 6, 5, 4, 3, 2, 1 ];
+console.log("Direct array prienting");
+console.log(arr);
 
-for(let i=0; i<10; i++){
-    console.log(handle());
+console.log("For each loop");
+
+arr.forEach(element => {
+    console.log(element);
+    
+});
+
+console.log("Using let variable");
+for(let i=0; i<arr.length; i++){
+    setTimeout(()=>{
+        console.log(arr[i]);
+        
+    }, 1000)
     
 }
 
-
+console.log("Using var variable");
+for(var i=0; i<arr.length; i++){
+    setTimeout(()=>{
+        console.log(arr[i]);
+        
+    }, 1000)
+    
+}
