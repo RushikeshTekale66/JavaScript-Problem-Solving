01b938fa0e003dfc6492fef80733aa0d4ac246a9
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-22 10:18:42 +0530

    Higher Order Function

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-(function(){
-    console.log("Addition is : ", (10+10));
-    
-})()
+function add(){
+    return function(){
+        console.log("Addition is : ", (10+10));
+    }
+}
+
+const result = add();
+
+//Higher order function
+result();
