0aa90676c8bdc3c1d33f8df383cff6553f13e59b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-05-24 18:15:38 +0530

    Reverse the string using the methods

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function add(...args){
-    let result = 0;
-    for(let i=0; i<args.length; i++){
-        result = result + args[i];
-    }
-    console.log(result);
-    
-}
+let str = 'Rushikesh';
 
-add(2, 3, 4, 5, 6, 6);
+console.log(str.split('').reverse().join(''));
