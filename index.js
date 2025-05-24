c50aa62f32cf02bfc415d812ced3aaa77b3749a4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-05-24 18:19:04 +0530

    Duplicates in string

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let str = 'Rushikesh';
 
-console.log(str.split('').reverse().join(''));
+for(let i=0; i<str.length; i++){
+    for(let j=i+1; j<str.length; j++){
+        if(str.charAt(i)==str.charAt(j)){
+            console.log(str.charAt(i));
+            
+        }
+    }
+}
